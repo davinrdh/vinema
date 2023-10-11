ea161c0629c47fafa8c91cdc38dfed68b2b33b68
davinrdh <davinridhas@gmail.com>
2023-10-11 10:57:24 +0700

    update

--- src/components/CardMovie.tsx
+++ src/components/CardMovie.tsx
@@ -1,8 +1,9 @@
 import { useNavigate } from "react-router-dom";
-import '../styles/CardMovie.scss'
 import Skeleton from 'react-loading-skeleton'
 import 'react-loading-skeleton/dist/skeleton.css'
 import { useEffect, useState } from "react";
+import { Card } from "react-bootstrap";
+import styled from "styled-components";
 
 interface ICardMovie {
   movie: any,
@@ -26,7 +27,7 @@ const CardMovie = ({ movie }: ICardMovie) => {
   return (
     <>
       {emulator &&
-        <div className="movie card" onClick={() => navigate(`/movie/${movie.id}`)}>
+        <CardStyled onClick={() => navigate(`/movie/${movie.id}`)}>
           <div className="release">{movie?.release_date.slice(0, 4)}</div>
           {emulatorImg &&
             <img src={`${import.meta.env.VITE_APP_BASEIMG}/${movie?.poster_path}`} alt="" />}
@@ -40,7 +41,7 @@ const CardMovie = ({ movie }: ICardMovie) => {
           </div>
           <div className="movie-title text-truncate">{movie.title}</div>
           <div className="movie-rate"><i className="bi bi-star-fill"></i> {movie.vote_average} </div>
-        </div>
+        </CardStyled>
       }
       {
         !emulator &&
@@ -52,3 +53,87 @@ const CardMovie = ({ movie }: ICardMovie) => {
 
 
 export default CardMovie
+
+const CardStyled = styled(Card)`
+      width: 170px;
+      height: 300px;
+      cursor: pointer;
+      overflow: hidden;
+      background-color: transparent;
+      color: var(--white);
+      transition: .2s ease;
+      text-align: center;
+
+      img {
+          border-radius: 8px;
+      }
+
+      .release {
+          position: absolute;
+          width: 35px;
+          height: 15px;
+          font-size: 10px;
+          border-radius: 0 0 8px 0;
+          background-color: red;
+      }
+
+      .bi-star-fill {
+          color: yellow;
+      }
+
+      .movie-title {
+          // padding: 5px;
+          height: 20px;
+          font-size: 14px;
+          margin-top: 5px;
+      }
+
+      .movie-rate {
+          font-size: 12px;
+          font-weight: 400;
+      }
+
+      img {
+          transition: 0.5s ease;
+      }
+
+      p {
+          margin: 0;
+          padding: 0;
+      }
+
+      .hover-effect {
+          opacity: 0;
+          padding: 2px;
+          // background-color: #3F2E3E;
+          position: absolute;
+          top: -30px;
+      }
+
+      &:hover {
+          transform: scale(1.05);
+
+          .hover-effect {
+              opacity: 1;
+          }
+      }
+
+      .overlay {
+          position: absolute;
+          top: 100%;
+          opacity: 0;
+          width: 100%;
+          height: 50%;
+          transition: 0.5s ease;
+          color: whitesmoke;
+
+          .movie-title {
+              font-weight: 700;
+          }
+
+          .bi-star-fill {
+              color: yellow;
+          }
+      }
+
+`
